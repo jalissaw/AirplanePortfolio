5687d0d3250226ba2bfaf5d9b1e5f0a92b96533a
jalissaw <57571010+jalissaw@users.noreply.github.com>
2020-05-26 11:53:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const toggler = document.querySelector('.toggler'); 
 const dropMenu = document.querySelector('.showmenu');
 const showLinks = document.querySelectorAll('.showlink');
-const showLink = document.querySelector('.showlink a');
+const showLink = document.querySelectorAll('.showlink a');
 const cards = document.querySelectorAll('.card')
 const maintain = document.querySelectorAll('.maintain')
 
 
-
+//Toggler Menu Button
 toggler.addEventListener('click',  () => {
     clicked = true;
     console.log('clicked');
     dropMenu.classList.toggle('open');
    
-    if (showLink === clicked) {
-        dropMenu.classList.remove('open');
-    } 
 
     //Let li items come in one at a time
     i = 0;
@@ -22,16 +19,34 @@ toggler.addEventListener('click',  () => {
         if(i < showLinks.length) {
             showLinks[i].classList.add('open');
             i++
-            setTimeout(showList, 500)
+            setTimeout(showList, 250)
         } 
         else i = 0;
     }
     showList();
 });
 
+//Close Menu on the click of a link
+function closeMenu () {
+    for (let i = 0; i < showLink.length; i++) {
+        showLink[i].addEventListener('click', function(){
+            console.log(showLink);
+            dropMenu.classList.remove('open');
+        })
+    }
+}
+
+closeMenu()
+
+
+
+
+
+
+
+
 // Project card fade in when scrolled 
 function scrolled () {
-    console.log(cards)
     for (var i = 0; i < cards.length; i++) {
     const position = cards[i].getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1.2;
